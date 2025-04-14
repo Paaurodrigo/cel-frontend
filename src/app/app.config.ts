921a2179260f658fb796c4c9a12a68ea11f8a8c3
Pau <paurodrigo2005@gmail.com>
2025-04-14 19:02:48 +0200

    config antigua

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,39 +1,16 @@
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { HTTP_INTERCEPTORS, HttpClientModule, HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { JWTInterceptor } from './interceptor/jwt.interceptor';
-import { AngularSignaturePadModule } from '@almothafar/angular-signature-pad'; // ✅ Módulo de firma digital
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
-// 🌍 Función para cargar archivos de traducción
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-}
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    provideHttpClient(),
-    provideAnimationsAsync(),
-    provideHttpClient(withInterceptorsFromDi()),
-    { provide: HTTP_INTERCEPTORS, useClass: JWTInterceptor, multi: true },
-
-    // 📌 Importa módulo firma digital
-    importProvidersFrom(
-      AngularSignaturePadModule,
-      HttpClientModule, // Necesario para la carga de traducciones
-      TranslateModule.forRoot({
-        loader: {
-          provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-          deps: [HttpClient]
-        },
-        defaultLanguage: 'es' // 🇪🇸 Idioma por defecto
-      })
-    )
-  ]
+    provideHttpClient(), provideAnimationsAsync(), provideAnimationsAsync(),
+    provideHttpClient( withInterceptorsFromDi() ),
+    { provide: HTTP_INTERCEPTORS, useClass: JWTInterceptor, multi: true }
+  ],
 };
