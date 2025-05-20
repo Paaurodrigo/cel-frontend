c229373f28e092b5f1890eaee6552be3c8674080
Pau <paurodrigo2005@gmail.com>
2025-05-20 16:34:19 +0200

    clase

--- src/app/shared/shared.home.routed/shared.home.routed.component.ts
+++ src/app/shared/shared.home.routed/shared.home.routed.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import L from 'leaflet';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-shared.home.routed',
@@ -7,14 +12,39 @@ import { TranslateModule } from '@ngx-translate/core';
   styleUrls: ['./shared.home.routed.component.css'],
   standalone: true, // ✅ Esto es clave para componentes standalone
   imports: [
-    TranslateModule
+    TranslateModule,
+    MatCardModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule
+
   ]
 })
 export class SharedHomeRoutedComponent implements OnInit {
+  resumen = [
+    { title: 'Productos', value: 152, icon: 'inventory' },
+    { title: 'Proveedores', value: 45, icon: 'groups' },
+    { title: 'Inmuebles', value: 18, icon: 'home_work' },
+    { title: 'Conexiones', value: 88, icon: 'hub' },
+  ];
+
+  ultimosProductos = [
+    { nombre: 'Tomate triturado', categoria: 'Conservas', imagenUrl: '/assets/img1.jpg' },
+    { nombre: 'Aceite virgen', categoria: 'Aceites', imagenUrl: '/assets/img2.jpg' },
+  ];
+
+
+  initMap() {
+    const map = L.map('map').setView([39.5, -0.4], 6);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+    // Añadir marcadores según instalaciones
+  }
 
   constructor() { }
 
+ 
   ngOnInit() {
+    this.initMap(); // puedes cargar leaflet si usas mapa
   }
 
 }
