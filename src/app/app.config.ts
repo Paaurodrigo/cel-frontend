ed779e1cb25e612773ca28ccd36ac23060c3fc37
Pau <paurodrigo2005@gmail.com>
2025-06-13 15:45:55 +0200

    idioma cambiado

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,23 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { JWTInterceptor } from './interceptor/jwt.interceptor';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+
+// Registrar el locale ES
+registerLocaleData(localeEs);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    provideHttpClient(), provideAnimationsAsync(), provideAnimationsAsync(),
-    provideHttpClient( withInterceptorsFromDi() ),
-    { provide: HTTP_INTERCEPTORS, useClass: JWTInterceptor, multi: true }
+    provideHttpClient(), 
+    provideAnimationsAsync(),
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: HTTP_INTERCEPTORS, useClass: JWTInterceptor, multi: true },
+    { provide: LOCALE_ID, useValue: 'es-ES' }  // <-- Aquí añades esto
   ],
 };
