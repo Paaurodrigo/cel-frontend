ac0017c35d7c031f82b33f3d9d8b2dd9f33ec1c7
Pau <paurodrigo2005@gmail.com>
2025-06-14 13:25:16 +0200

    view mejorado

--- src/app/component/conexion/conexion.admin.view.routed/conexion.admin.view.routed.component.ts
+++ src/app/component/conexion/conexion.admin.view.routed/conexion.admin.view.routed.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { IConexion } from '../../../model/conexion.interface';
 import { ConexionService } from '../../../service/conexion.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-conexion.admin.view.routed',
   templateUrl: './conexion.admin.view.routed.component.html',
-  styleUrls: ['./conexion.admin.view.routed.component.css']
+  styleUrls: ['./conexion.admin.view.routed.component.css'],
+  imports:[ RouterModule, CommonModule],
+  standalone: true
 })
 export class ConexionAdminViewRoutedComponent implements OnInit {
   id: number = 0;
